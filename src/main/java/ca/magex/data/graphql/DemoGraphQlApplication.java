2895aac44204e7efc03d2bc44422c80a87602cd5
magex9 <scott@magex.ca>
2020-03-04 14:25:55 -0500

    Simplified

--- src/main/java/ca/magex/data/graphql/DemoGraphQlApplication.java
+++ src/main/java/ca/magex/data/graphql/DemoGraphQlApplication.java
@@ -1,18 +1,11 @@
 package ca.magex.data.graphql;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import ca.magex.data.api.repositories.Repository;
 import ca.magex.data.datastore.repositories.DataStoreRepository;
-import graphql.ExceptionWhileDataFetching;
-import graphql.GraphQLError;
-import graphql.servlet.GraphQLErrorHandler;
 
 @SpringBootApplication
 public class DemoGraphQlApplication {
@@ -21,29 +14,6 @@ public class DemoGraphQlApplication {
 		SpringApplication.run(DemoGraphQlApplication.class, args);
 	}
 
-	@Bean
-	public GraphQLErrorHandler errorHandler() {
-		return new GraphQLErrorHandler() {
-			@Override
-			public List<GraphQLError> processErrors(List<GraphQLError> errors) {
-				List<GraphQLError> clientErrors = errors.stream().filter(this::isClientError)
-						.collect(Collectors.toList());
-
-				List<GraphQLError> serverErrors = errors.stream().filter(e -> !isClientError(e))
-						.map(GraphQLErrorAdapter::new).collect(Collectors.toList());
-
-				List<GraphQLError> e = new ArrayList<>();
-				e.addAll(clientErrors);
-				e.addAll(serverErrors);
-				return e;
-			}
-
-			protected boolean isClientError(GraphQLError error) {
-				return !(error instanceof ExceptionWhileDataFetching || error instanceof Throwable);
-			}
-		};
-	}
-	
 	@Bean
 	public Repository repo() {
 		return new DataStoreRepository();
